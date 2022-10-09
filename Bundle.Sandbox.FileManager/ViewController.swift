6c40aa12c9d83dcaa8180f35afafebca8d867270
Artem Sviridov <exastronaut@yandex.ru>
2022-10-09 15:32:12 +0300

    added project

---
+++ Bundle.Sandbox.FileManager/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Bundle.Sandbox.FileManager
+//
+//  Created by Артем Свиридов on 09.10.2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+

4c85df9bff6297cb3c56fad884bf591572b3bafd
Artem Sviridov <exastronaut@yandex.ru>
2022-10-27 15:11:05 +0300

    created an image gallery, added the ability to add and delete images

---
+++ Bundle.Sandbox.FileManager/Sources/Gallery screen/Entity/ImageModel.swift
@@ -0,0 +1,15 @@
+//
+//  ImageModel.swift
+//  Bundle.Sandbox.FileManager
+//
+//  Created by Артем Свиридов on 27.10.2022.
+//
+
+import UIKit
+
+struct ImageModel {
+
+    var path: String?
+    var image: UIImage?
+    
+}
